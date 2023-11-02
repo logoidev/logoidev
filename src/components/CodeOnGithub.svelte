12d9c334d73b8e24c5ea282772d812642381d66d
Vlad Kolbaia <v@logoi.dev>
2023-11-02 05:37:44 +0000

    Add CodeOnGithub button base

---
+++ src/components/CodeOnGithub.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const GITHUB_REPO_URL_BASE = 'https://github.com/logoidev/logoidevg';
+	const GITHUB_REPO_URL = `${GITHUB_REPO_URL_BASE}/tree/main/src/`;
+	const GITHUB_LOGO_URL =
+		'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg';
+
+    export let path = "";
+	$: url = GITHUB_REPO_URL + path;
+</script>
+
+<div>
+	<img alt="GitHub logo" src={GITHUB_LOGO_URL} />
+	<a href={url}>View code on GitHub</a>
+</div>
+
+<style>
+	div {
+		position: absolute;
+		display: flex;
+		align-items: center;
+		bottom: 0;
+		right: 0;
+		margin: 8px;
+	}
+	img {
+		width: 24px;
+		margin: 8px;
+	}
+
+	@media only screen and (max-width: 450px) {
+		a {
+			display: none;
+		}
+	}
+</style>
