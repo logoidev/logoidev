af99fc7475f5ed471725abeec912220eaaf2e815
Vlad Kolbaia <v@logoi.dev>
2023-11-03 14:43:18 -0400

    Vision article review
    Some of #4 is adressed

--- src/routes/blog/content/vision.svelte
+++ src/routes/blog/content/vision.svelte
@@ -2,7 +2,7 @@
 	<p class="text-center mb-2">Welcome to our blog!</p>
 	<p>
 		This is an introductory post where I wanted to shed some light on this yet very small collective
-		we've established, our background, motivations and goals.
+		we've established - our background, motivations, and goals.
 	</p>
 
 	<p>
@@ -14,7 +14,7 @@
 
 	<p>
 		Together with my brother <i>Andrew</i> and our friend <i>Adam</i> we're looking to explore new ways
-		to learn, educate, and use technology to inspire, wow and uplift people beyond utilitarian convenience
+		to learn, educate, and use technology to inspire, wow, and uplift people beyond utilitarian convenience
 		of technology.
 	</p>
 
@@ -26,21 +26,21 @@
 	</p>
 
 	<p>
-		As an engineer in this modern technological world I've been granted quite a bit of power and
+		As an engineer in this modern technological world, I've been granted quite a bit of power and
 		ability, yet I felt empty and directionless. Building digital products and startups is extremely
-		fun and fulfilling, and yes - it does pay well, but what is next? Will the fruits of my labour
+		fun and fulfilling, and yes - it does pay well, but what is next? Will the fruits of my labor
 		be valuable not just for me but after I'm gone? I'd love to think there is a path to answer
-		these questions positively. Time and time again I remember the words to my favourite super-hero
+		these questions positively. Time and time again I remember the words spoken to my favorite super-hero
 		by his dying <i>father</i>... Yet we seem to be running away from responsibility, or what could
 		be worse, taking on responsibility that is not ours and often failing at that as well.
 	</p>
 
 	<p>
 		The world right now is in a very dangerous and unstable state. Confusion, panic, anxiety,
-		overwhelming amounts of information, aimlessness, radicalisation, and inability to distinquish
+		overwhelming amounts of information, aimlessness, radicalization, normophobia, and inability to distinguish
 		what is true is at the core — and technology seems to make it all even worse. Yet I am convinced
-		it doesn't necessarily have to. <i>"Making a world a better place"</i> is somewhat of a joke in the
-		tech community, but I hope there's enough of us there who take it seriously.
+		it doesn't necessarily have to. <i>"Making the world a better place"</i> is somewhat of a joke in the
+		tech community, but I hope there are enough of us there who take it seriously.
 	</p>
 
 	<p>
@@ -48,25 +48,25 @@
 		power over our lives in the future, is that future actually good and hopeful? Do the machines
 		know what is good or bad, are they even capable, and more importantly - do the people who build
 		them know? Can we use progress to prevent poverty, hunger and <b>war</b>, rather than cause
-		them? I've come to believe that while science and technology is undoubtedly extremely powerful,
-		there are things beyond the rational world that are true, valuable and absolutely necessary for
-		us to live a good and prosperous lives, individually and collectively.
+		them? I've come to believe that while science and technology are undoubtedly extremely powerful,
+		there are things beyond the rational world that are true, valuable, and absolutely necessary for
+		us to live good and prosperous lives, individually and collectively.
 	</p>
 
 	<p>
-		What about art, literature, the stories we tell and ethics we share? Rationalism is quite
+		What about art, literature, the stories we tell, and ethics we share? Rationalism is quite
 		unfulfilling if you take it seriously, and I’d say we have taken it way too seriously, to our
-		own detriment. How can you not learn anything from the horrors of twisted-rationalism of Marxist
+		own detriment. How can you not learn anything from the horrors of twisted rationalism of Marxist
 		ideology? Or those of fascism. I’m convinced that the scientific and rational truth is not the
-		only type of truth the order is built upon, and quite arguably the unknowable and unprovable is
+		the only type of truth the order is built upon, and quite arguably the unknowable and unprovable is
 		far more important than science.
 	</p>
 
 	<p>
-		My favorite work of art is the Sistine Chapel — I dream to one day see it with my own eyes and
+		My favorite work of art is the Sistine Chapel — I dream of one day seeing it with my own eyes and
 		show it to my son. But how was it made? Is it built on pure science and engineering? Definitely
-		thats a big part — it had been beautifully designed by some brilliant architects of the
-		Reneisance age, and built by the hands of the people whose names are unknown to most. But what
+		that is a big part — it had been beautifully designed by some brilliant architects of the
+		Renaissance age, and built by the hands of the people whose names are unknown to most. But what
 		of the works of art decorating the walls? They are known worldwide and made by the most
 		brilliant artists whom we have never surpassed. It's much more than layers of paint on the walls
 		— at least we all act like it is, and travel great distances for a chance to see it.
@@ -77,35 +77,35 @@
 		study and perfect his mastery to craft such beauty? Why do we even perceive it as beautiful? I
 		can only explain it with divine inspiration and faith. <i
 			>"I saw the angel in the marble and carved until I set him free."</i
-		>. Have you ever seen an angel let alone freed one? Of course I will never be Michelangelo, but
+		>. Have you ever seen an angel let alone freed one? Of course, I will never be Michelangelo, but
 		I can only try to follow in his steps and possibly inspire a new one to be born, educated and
 		encouraged to pursue their craft to the extent it is possible.
 	</p>
 
 	<p>
-		It hurts to realise that we have not seen anything close to such beauty produced since the
+		It hurts to realize that we have not seen anything close to such beauty produced since the
 		Renaissance. Why have we lost access to such inspiration while gaining so much technological
 		power? Why have we lost the charity of spirit to finance such things and devotion to something
 		so beyond us and on a scale our ancestors did? Could it be they were actually much smarter and
-		wiser than we are? Of course these are rethorical questions — but I think most people would
+		wiser than we are? Of course, these are rhetorical questions — but I think most people would
 		agree we did lose <i>something</i>.
 	</p>
 
 	<p>
-		Growing up in a soviet-built boxed utilitarian beehives the only beauty I saw in architecture
+		Growing up in a Soviet-built boxed utilitarian beehive the only beauty I saw in architecture
 		were churches, as even the monuments were reflective of the soviet doctrines. Orthodox churches
-		and Chirstian art, are among the only places that were preserved and still hold beauty as
-		something valuable, at least in Eastern Europe. After coming to the West, I glipmsed this too,
-		but it is so rare. Apart from an occasional beautiful family house I see copy-pasted spread out
-		suburbs, like a huge shopping mall that needs you be put in metal boxes on wheels to get
+		and Christian art, are among the only places that were preserved and still hold beauty as
+		something valuable, at least in Eastern Europe. After coming to the West, I glimpsed this too,
+		but it is so rare. Apart from an occasional beautiful family house, I see copy-pasted spread out
+		suburbs, like a huge shopping mall that needs you to be put in metal boxes on wheels to get
 		anywhere. The West, apart from the legacy left by our inspired European ancestors, lacks such
 		communal places in comparison. Very few churches are the exception, at least here in Canada,
 		like The Cathedral Church of St. James in Toronto with beautiful stained glass icons.
 	</p>
 
 	<p>
-		Now our art and beauty is challenged, disrespected and even intentionally destroyed like a
-		tragedy of Notre-Dame fire in 2019. So how do we protect it, preserve it and spread it? Do we
+		Now our art and beauty are challenged, disrespected, and even intentionally destroyed like a
+		tragedy of Notre-Dame fire in 2019. So how do we protect it, preserve it, and spread it? Do we
 		have what it takes to build anew?
 	</p>
 
@@ -115,16 +115,16 @@
 	</p>
 
 	<p>
-		I wrote my masters paper on the topic of ethical computing, which has come to be even more
-		relevant with the recent advances in AI and its consumer adoption. More power is given to these
+		I wrote my master's paper on the topic of ethical computing, which has come to be even more
+		relevant to the recent advances in AI and its consumer adoption. More power is given to these
 		systems mindlessly, which could be a race to the grave. How can we teach the machine what is
 		good and moral if we do not know ourselves? Can we continue to combine the wisdom and
-		inspiration of our fathers with the blessings of technology, science and progress, or are they
+		inspiration of our fathers with the blessings of technology, science, and progress, or are they
 		just incompatible?
 	</p>
 	<p>
-		One thing prgoramming teaches you is the importance of abstraction and reliance on solid
-		foundations to built anything that works. We are so accustomed to the miracle that things always
+		One thing programming teaches you is the importance of abstraction and reliance on solid
+		foundations to build anything that works. We are so accustomed to the miracle that things always
 		work - electricity, water, heat, accessible food, sewage, the internet, banks, open borders, <i
 			>freedom...</i
 		>
@@ -140,10 +140,10 @@
 	<pre class="text-sm">// Here be DRAGONS </pre>
 
 	<p>
-		It is comical how true it is. Usually it indicates that the code was initially poorly written,
+		It is comical how true it is. Usually, it indicates that the code was initially poorly written,
 		or has grown in complexity over time so that is now hard to understand, but it <i>works</i> and
 		it's <b>important</b> - so mess with it at your own peril. Same with culture - we must know what
-		is true, solid and working, and protect it at all cost.
+		is true, solid, and working, and protect it at all costs.
 	</p>
 
 	<p>
@@ -153,33 +153,33 @@
 		> and I think it reflects the goal to bring together many different and independently good things
 		into something coherent and better. I hope to combine my continued education in liberal arts with
 		my skills and interests in the most advanced and exciting technologies I can think of, which for
-		me are design, software engineering, web development, UI, spatial computing, web3 and AI.
+		me are design, software engineering, web development, UI, spatial computing, web3, and AI.
 	</p>
 
 	<p>
 		There are great many examples of technological achievement and success. Like the printing press,
 		electromagnetism, or the splitting of an atom. Even today I see technology used to solve real
-		issues and make people's lives better daily. Personally I regard Google Maps a technological
-		wonder, so are YouTube, Google Search and of course Apple. I am inspired by their creators just
+		issues and make people's lives better daily. Personally, I regard Google Maps as a technological
+		wonder, so are YouTube, Google Search, and of course Apple. I am inspired by their creators just
 		as much as I am inspired by Leonardo and his peers. I can not mention all the giants on whose
 		shoulders we collectively stand as there are too many, but physicists, mathematicians and
 		engineers have discovered many wonders of the world, including the Promethean power of the
 		nuclear energy. But
-		<i>techne</i> can also be used to bring geat sorrow — due to malevolence, corruption, inaction or
+		<i>techne</i> can also be used to bring great sorrow — due to malevolence, corruption, inaction or
 		even by mistake and untruthful craft. As a Ukrainian, Chernobyl is a story engraved in my soul, showing
-		the true dangers of science, technology and engineering gone horribly wrong. So let's try to be more
+		the true dangers of science, technology, and engineering gone horribly wrong. So let's try to be more
 		careful and truthful, both in our work and with each other.
 	</p>
 
 	<p>
 		We wish to continue in the steps of great people of the past and align technology with the
-		concepts of ethics, morality and beauty, so please pray that we can do that to the best of our
+		concepts of ethics, morality, and beauty, so please pray that we can do that to the best of our
 		abilities.
 	</p>
 
 	<p>
 		We really hope you are interested in helping us learn and find ways to apply and unite these
-		interests, and we welcome any feedback, advice, ideas, criticism and all new acquaintances along
+		interests, and we welcome any feedback, advice, ideas, criticism, and all new acquaintances along
 		the way.
 	</p>
 	<br />
