b2613685ce69478ce116c11a5b0b8daa3fc9048d
Vlad Kolbaia <vlad.kolbaia@gmail.com>
2024-01-28 20:03:40 -0500

    Attempt to fix Vercel DB build

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,11 +1,16 @@
 import 'reflect-metadata';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import { DataSource } from 'typeorm';
 import { CoinModel } from './entity/coin/Coin.model';
 import { LocationModel } from './entity/location/Location.model';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const AppDataSource = new DataSource({
 	type: 'sqlite',
-	database: 'database.sqlite',
+	database: path.resolve(__dirname, '..', '..', '..', 'database.sqlite'),
 	synchronize: true,
 	logging: false,
 	entities: [CoinModel, LocationModel],
