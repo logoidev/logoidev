5e7bfa0a8c7c2117efcfc142861273d150ae5dfb
v1adko <vlad@petersonacademy.com>
2025-03-02 02:52:14 -0500

    feat: adam direct link

---
+++ src/lib/analytics/posthog.server.ts
@@ -0,0 +1,27 @@
+import { PostHog } from 'posthog-node';
+
+import { PUBLIC_POSTHOG_API_TOKEN } from '$env/static/public';
+import type { Cookies } from '@sveltejs/kit';
+
+const getPosthogInstance = () =>
+	new PostHog(PUBLIC_POSTHOG_API_TOKEN, { host: 'https://us.i.posthog.com' });
+
+const posthog = getPosthogInstance();
+
+const getPosthogCookie = (cookies: Cookies) =>
+	JSON.parse(cookies.get(`ph_${PUBLIC_POSTHOG_API_TOKEN}_posthog`) ?? '{}') as {
+		distinct_id?: string;
+	};
+
+export const getDistinctId = (cookies: Cookies) => {
+	const posthogCookie = getPosthogCookie(cookies);
+	return posthogCookie.distinct_id; // Skipping uuidv7 generation as a fallback for now
+};
+
+type TrackingServerEvent = 'adam_direct_link_visited';
+
+export function trackServerEvent(eventName: TrackingServerEvent, cookies: Cookies) {
+	const distinctId = getDistinctId(cookies) ?? 'fallback-distinct-id';
+	console.log('Tracking', distinctId, eventName);
+	posthog.capture({ distinctId, event: eventName });
+}
---
+++ src/routes/adam/+page.server.ts
@@ -0,0 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+
+import { trackServerEvent } from 'src/lib/analytics/posthog.server';
+
+const ADAM_CUSTOM_CHAT_GPT_DIRECT_LINK =
+	'https://chatgpt.com/g/g-67bf48c4932881918f63f2197d68ec57-adam';
+
+export function load({ cookies }) {
+	trackServerEvent('adam_direct_link_visited', cookies);
+	redirect(303, ADAM_CUSTOM_CHAT_GPT_DIRECT_LINK);
+}
