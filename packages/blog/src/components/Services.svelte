5e7b96e643601dd67a92ed22ca1c51fbae61318f
v1adko <vlad@logoi.dev>
2025-07-11 17:24:55 -0400

    fix: pyramid padding

--- packages/blog/src/components/Services.svelte
+++ packages/blog/src/components/Services.svelte
@@ -28,7 +28,7 @@
 
 <div
 	role="presentation"
-	class={clsx('button relative mt-12 mb-6 flex items-center justify-center', [
+	class={clsx('button relative mt-10 mb-6 flex items-center justify-center', [
 		isShown ? 'shown' : 'h-8',
 		isGolden ? 'golden' : ''
 	])}
