e9a3031448998dbc23aeeac1941b98e46c2f8a07
v1adko <vlad@logoi.dev>
2025-07-14 22:08:48 -0400

    change env import

--- packages/blog/src/db/mongodb.ts
+++ packages/blog/src/db/mongodb.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
+import { SECRET_MONGODB_URI } from '$env/static/private';
 
-const MONGODB_URI = process.env.SECRET_MONGODB_URI;
+const MONGODB_URI = SECRET_MONGODB_URI;
 const DB_NAME = 'logoi';
 
 let client: MongoClient | null = null;
@@ -12,7 +13,7 @@ export const getMongoClient = async (): Promise<MongoClient> => {
 				throw new Error('MONGODB_URI is not set');
 			}
 
-			console.log('Connecting to MongoDB:', MONGODB_URI);
+			console.log('Connecting to MongoDB:', MONGODB_URI.slice(0, 20) + '...');
 			client = new MongoClient(MONGODB_URI);
 			await client.connect();
 			console.log('MongoDB connected successfully');
